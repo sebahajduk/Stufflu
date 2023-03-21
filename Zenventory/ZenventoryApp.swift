3ad71fa100c1bf0682a39567b95a953102ad14ec
Sebastian Hajduk <jiuzeer@gmail.com>
2023-03-21 05:46:13 +0100

    Initial Commit

---
+++ Zenventory/ZenventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenventoryApp.swift
+//  Zenventory
+//
+//  Created by Sebastian Hajduk on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZenventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
